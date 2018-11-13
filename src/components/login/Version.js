d44128a3411328b5eab6f96ab6fc8b92519f1cb3
roneymaia <roneymaia@gmail.com>
2018-11-13 17:20:49 -0200

    Versao 1-3-6

--- src/components/login/Version.js
+++ src/components/login/Version.js
@@ -43,7 +43,7 @@ class Version extends Component {
     componentDidMount() {
         this.props.iniciaTela();
 
-        const version = '1.3.5';
+        const version = '1.3.6';
         
         this.props.modificaVersao(version);
         this.props.verificaConexao();
